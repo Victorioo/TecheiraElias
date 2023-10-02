7655b8231f93f87db53ee36adc0affbf05abfc9b
Victorio <victoriocaccioppoli@gmail.com>
2023-10-02 20:28:15 -0300

    added: metadata

--- app/page.jsx
+++ app/page.jsx
@@ -3,6 +3,12 @@ import About from "@/components/About";
 import HowWork from "@/components/HowWork";
 import Experience from "@/components/Experience";
 import Contact from "@/components/Contact";
+import { Metadata } from "next";
+
+export const metadata = {
+  title: "Elias Techeira",
+  description: "Portfolio de Elias Techeira, Maestro mayor de obras, estudiante de arquitectura y modelador 3D",
+};
 
 export default function page() {
   return (
